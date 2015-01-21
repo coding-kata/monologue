5653fd6ce965b1531696a54d4dd6024eb508fa19
azu <azuciao@gmail.com>
2015-01-22 01:46:55 +0900

    refactor(view): move over to Backbone.js views

--- public/js/Views/NewStatusView.js
+++ public/js/Views/NewStatusView.js
@@ -5,27 +5,29 @@
  */
 var emitter = require("../Dispatcher/status-dispatcher");
 var action = require("../Constants/status-constant");
-function NewStatusView(options) {
-    var that = this;
-    this.statuses = options.statuses;
-    emitter.on(action.STATUS_ADD, this.clearInput, this);
-    this.el = options.el;
-    this.el.on('submit', function (event) {
-        event.preventDefault();
-        that.addStatus(event);
-    });
-}
-NewStatusView.prototype.addStatus = function (event) {
-    var that = this;
-    this.statuses.add({
-        text: that.el.find("textarea").val(),
-        success: function (data) {
-            emitter.trigger(action.STATUS_ADD, data.text);
-        }
-    });
-};
-NewStatusView.prototype.clearInput = function clearInput() {
-    this.el.val("");
-};
+var NewStatusView = Backbone.View.extend({
+    initialize: function (options) {
+        var that = this;
+        this.statuses = options.statuses;
+        emitter.on(action.STATUS_ADD, this.clearInput, this);
+        this.el = options.el;
+        this.el.on('submit', function (event) {
+            event.preventDefault();
+            that.addStatus(event);
+        });
+    },
+    addStatus: function () {
+        var that = this;
+        this.statuses.add({
+            text: that.el.find("textarea").val(),
+            success: function (data) {
+                emitter.trigger(action.STATUS_ADD, data.text);
+            }
+        });
+    },
+    clearInput: function clearInput() {
+        this.el.val("");
+    }
+});
 
 module.exports = NewStatusView;
--- public/js/Views/StatusesView.js
+++ public/js/Views/StatusesView.js
@@ -5,12 +5,14 @@
  */
 var emitter = require("../Dispatcher/status-dispatcher");
 var action = require("../Constants/status-constant");
-function StatuesView(options) {
-    this.el = options.el;
-    emitter.on(action.STATUS_ADD, this.appendStatus, this);
-}
-StatuesView.prototype.appendStatus = function (text) {
-    this.el.append('<li>' + text + '</lib>');
-};
+var StatuesView = Backbone.View.extend({
+    initialize: function (options) {
+        this.el = options.el;
+        emitter.on(action.STATUS_ADD, this.appendStatus, this);
+    },
+    appendStatus: function (text) {
+        this.el.append('<li>' + text + '</lib>');
+    }
+});
 
 module.exports = StatuesView;
